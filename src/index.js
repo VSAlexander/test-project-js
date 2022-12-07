c27f9c2de6e8054fa820cbf72e17a4c62005bbed
VSAlexander <shevchenkoav.97@gmail.com>
2022-12-07 20:55:33 +0200

    final fix

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ const API_KEY = '2954afe7c35181c36bf30aa4bc9ce527';
 
 /////////    GENRES     /////////
 
+console.log('Hello');
+
 async function fetchGenres() {
-  // if (localStorage.getItem('genres') !== null) {
-  //   return;
-  // }
+  if (localStorage.getItem('genres') !== null) {
+    return;
+  }
   try {
     const response = await axios.get(
       `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
