eea3a6ea0202bbd20ea6df06e7bc8988a48eb405
VSAlexander <shevchenkoav.97@gmail.com>
2022-12-07 20:36:08 +0200

    fixed

--- src/renderMovieCards.js
+++ src/renderMovieCards.js
@@ -1,48 +1,47 @@
-import { getGenresFromLocalStorage } from './fetchGenres';
+// import { getGenresFromLocalStorage } from './fetchGenres';
 
-export function renderMovieCards(data) {
-  return data
-    .map(
-      movie => `<li class="movies-list__item">
-      <div class="movies-list__item-thumb">
-        <img loading="lazy"
-          class="movies-list__item-card-img"
-          src="https://image.tmdb.org/t/p/w342${movie.poster_path}"
-          srcset="https://image.tmdb.org/t/p/w342${movie.poster_path} 1x,
-          https://image.tmdb.org/t/p/w780${movie.poster_path} 2x"
+// export function renderMovieCards(data) {
+//   return data
+//     .map(
+//       movie => `<li class="movies-list__item">
+//       <div class="movies-list__item-thumb">
+//         <img loading="lazy"
+//           class="movies-list__item-card-img"
+//           src="https://image.tmdb.org/t/p/w342${movie.poster_path}"
+//           srcset="https://image.tmdb.org/t/p/w342${movie.poster_path} 1x,
+//           https://image.tmdb.org/t/p/w780${movie.poster_path} 2x"
 
-          alt="${movie.title}">
-      </div>
-        
-          
-        <div class="movies-list__item-caption">
-            <h2 class="movies-list__item-title">${movie.title}</h2>
-            <p class="movies-list__item-info">${checkLengthOfGenres(
-              movie.genre_ids
-            )} | ${
-        movie.release_date.slice(0, 4)
-          ? movie.release_date.slice(0, 4)
-          : 'No info'
-      } <span class="rating">${movie.vote_average.toFixed(1)}</span></p>
-        </div>
-        </li>`
-    )
-    .join('');
-}
+//           alt="${movie.title}">
+//       </div>
 
-function checkLengthOfGenres(array) {
-  const allGenres = getGenresFromLocalStorage('genres')
-    .filter(genre => array.includes(genre.id))
-    .map(genre => genre.name);
+//         <div class="movies-list__item-caption">
+//             <h2 class="movies-list__item-title">${movie.title}</h2>
+//             <p class="movies-list__item-info">${checkLengthOfGenres(
+//               movie.genre_ids
+//             )} | ${
+//         movie.release_date.slice(0, 4)
+//           ? movie.release_date.slice(0, 4)
+//           : 'No info'
+//       } <span class="rating">${movie.vote_average.toFixed(1)}</span></p>
+//         </div>
+//         </li>`
+//     )
+//     .join('');
+// }
 
-  if (allGenres.includes('Science Fiction')) {
-    allGenres.splice(allGenres.indexOf('Science Fiction'), 1, 'Sci-Fi');
-  }
+// function checkLengthOfGenres(array) {
+//   const allGenres = getGenresFromLocalStorage('genres')
+//     .filter(genre => array.includes(genre.id))
+//     .map(genre => genre.name);
 
-  if (allGenres.length > 3) {
-    allGenres.splice(2, 1, 'Other');
-    return allGenres.slice(0, 3).join(', ');
-  } else {
-    return allGenres.slice(0, 3).join(', ');
-  }
-}
+//   if (allGenres.includes('Science Fiction')) {
+//     allGenres.splice(allGenres.indexOf('Science Fiction'), 1, 'Sci-Fi');
+//   }
+
+//   if (allGenres.length > 3) {
+//     allGenres.splice(2, 1, 'Other');
+//     return allGenres.slice(0, 3).join(', ');
+//   } else {
+//     return allGenres.slice(0, 3).join(', ');
+//   }
+// }
